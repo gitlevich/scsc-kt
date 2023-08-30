61300e5205a508e890ab122722af23da2496cb9c
Vladimir Gitlevich <vlad@xaoc.com>
2023-08-29 18:11:44 -0700

    Cleanup

--- scsc-monolith/src/main/kotlin/demo/scsc/config/AxonFramework.kt
+++ scsc-monolith/src/main/kotlin/demo/scsc/config/AxonFramework.kt
@@ -143,8 +143,10 @@ class AxonFramework private constructor(private val applicationName: String, pri
         private val LOCK = Any()
         private val LOG = LoggerFactory.getLogger(AxonFramework::class.java)
 
-        val appConfig: Config = ConfigFactory.load()
-        fun configure(name: String): AxonFramework = AxonFramework(name, appConfig)
+        val appConfig: Config by lazy { ConfigFactory.load() }
+
+        private fun appConfig() = appConfig
+        fun configure(name: String, appConfig: Config = appConfig()): AxonFramework = AxonFramework(name, appConfig)
         fun Config?.isInspectorAxonConfigured(): Boolean = (this != null
                 && hasPath("workspace")
                 && hasPath("environment")
